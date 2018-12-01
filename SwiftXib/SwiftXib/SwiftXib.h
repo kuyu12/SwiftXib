e7b600005a30371d8c8a0a887f633f06c9f919b1
Yair Hadad <kuyu12@gmail.com>
2018-12-01 20:45:32 +0200

    Create base  framework project

---
+++ SwiftXib/SwiftXib/SwiftXib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftXib.h
+//  SwiftXib
+//
+//  Created by yair hadad on 01/12/2018.
+//  Copyright © 2018 yair hadad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftXib.
+FOUNDATION_EXPORT double SwiftXibVersionNumber;
+
+//! Project version string for SwiftXib.
+FOUNDATION_EXPORT const unsigned char SwiftXibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftXib/PublicHeader.h>
+
+
